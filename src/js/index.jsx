00dc94a94407db3c36967687f7358ed8cc603949
Laura Comte <comtelaurapro@gmail.com>
2023-07-06 17:15:24 +0200

    practice

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,8 +1,19 @@
 const Container = () => {
+
+  const [inputValue, setinputValue] = React.useState("something")
+
+  const UpdateValue = (value) => {
+    setinputValue(value)
+  }
+
   return (
     <div className="container">
+      <input type="text" value={inputValue} onChange={(e) => UpdateValue(e.target.value)} />
       <div className="flex">
-        <YellowBlock width={200} height={200} greenWidth={0} greenHeight={0} />
+        <YellowBlock
+          width={200}
+          height={200}
+        />
 
         <YellowBlock
           width={200}
@@ -12,32 +23,55 @@ const Container = () => {
         />
       </div>
 
-      <BlueBlock width={600} height={300} />
-      <div className="flex-inred">
-        <RedCircle width={150} height={150} />
-      </div>
+      <BlueBlock inputValue={inputValue} />
+
+      <RedCircle size={200} />
     </div>
   );
 };
 
 const YellowBlock = (props) => {
   const { width, height, greenWidth, greenHeight } = props;
+  const [showGreenBlock, setShowGreenBlock] = React.useState(true);
+
+  const handleGreenBlockClick = () => {
+    setShowGreenBlock(false);
+  };
+
+  const handleClick = (target) => {
+    if (!target.classList.contains("yellow-block")) return;
+    setShowGreenBlock(true);
+  };
 
   return (
     <div
       className="yellow-block"
+      onClick={(e) => handleClick(e.target)}
       style={{ width: `${width}px`, height: `${height}px` }}
     >
-      <GreenBlock width={greenWidth} height={greenHeight} />
+      {greenWidth > 0 && greenHeight > 0 && showGreenBlock && (
+        <GreenBlock
+          width={greenWidth}
+          height={greenHeight}
+          onGreenBlockClick={() => handleGreenBlockClick()}
+        />
+      )}
     </div>
   );
 };
 
 const GreenBlock = (props) => {
-  const { width, height } = props;
+  const { width, height, onGreenBlockClick } = props;
+  // onGreenBlockClick = () => handleGreenBlockClick();
+
+  const handleClick = () => {
+    onGreenBlockClick();
+  };
 
   return (
     <div
+      onClick={(e) => handleClick()}
+      // onClick={onGreenBlockClick}
       className="green-block"
       style={{ width: `${width}px`, height: `${height}px` }}
     ></div>
@@ -45,34 +79,64 @@ const GreenBlock = (props) => {
 };
 
 const BlueBlock = (props) => {
-  const { width, height } = props;
+  const { inputValue } = props;
+  const [redColor, setRedColor] = React.useState(`hsl(0, 50%, 50%)`);
+
+  const handleRedCircleClick = (color) => {
+    console.log(color);
+    setRedColor(color);
+  };
 
   return (
-    <div
-      className="blue-block"
-      style={{ width: `${width}px`, height: `${height}px` }}
-    >
+    <div className="blue-block">
       <div className="flex-inblue">
         <YellowBlock
-          width={150}
-          height={150}
-          greenWidth={120}
-          greenHeight={120}
+          width={130}
+          height={130}
+          greenWidth={96}
+          greenHeight={96}
         />
-        <GreenBlock width={150} height={50} />
+
+        <GreenBlock
+          width={140}
+          height={12}
+        />
       </div>
-      <RedCircle width={150} height={150} />
+      <span>{inputValue}</span>
+      <p>{redColor}</p>
+      
+      <RedCircle
+        handleClickFromParent={(color) => handleRedCircleClick(color)}
+        size={140}
+      />
     </div>
   );
 };
 
 const RedCircle = (props) => {
-  const { width, height } = props;
+  const { size, handleClickFromParent } = props;
+  // const handleClickFromParent = (color) => {
+  //   handleRedCircleClick(color)
+  // }
+
+  const [color, setColor] = React.useState(0);
+
+  const handleClick = () => {
+    const rdm = Math.floor(Math.random() * 361);
+    setColor(rdm);
+    if (!handleClickFromParent) return;
+    handleClickFromParent(`hsl(${rdm}, 50%, 50%)`);
+  };
 
   return (
     <div
+      onClick={(e) => handleClick()}
       className="red-circle"
-      style={{ width: `${width}px`, height: `${height}px` }}
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        backgroundColor: `hsl(${color}, 50%, 50%)`,
+      }}
     ></div>
   );
 };
